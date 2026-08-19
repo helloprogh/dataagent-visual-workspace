5e65f048dc98497fbe2bb7e000c947001b17e9e4
sujinqu <1101485871@qq.com>
2026-08-19 14:29:16 +0800

    load three-zone shell styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@copilotkit/vue/styles.css'
 import './style.css'
+import './layout-shell.css'
 import App from './App.vue'
 
 createApp(App).use(ElementPlus).mount('#app')
