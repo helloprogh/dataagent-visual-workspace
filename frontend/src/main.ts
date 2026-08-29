85e2229fd5eb98459b1114e76e9442f5b4ce7dfd
sujinqu <1101485871@qq.com>
2026-08-29 17:47:57 +0800

    feat: initialize persisted theme before mount

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,6 +8,9 @@ import './layout-shell.css'
 import './app-navigation-pages.css'
 import './opencode-management.css'
 import './composer-model-placement.css'
+import './light-theme.css'
+import { initializeTheme } from './theme'
 import App from './App.vue'
 
+initializeTheme()
 createApp(App).use(ElementPlus).mount('#app')
